5ca99d3ab105d7feb506d556e678ddeb02ee7232
ImaniKadiva7 <105231479+ImaniKadiva7@users.noreply.github.com>
2022-10-18 09:23:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,37 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
-if(bar)
+setCookie = (cName, cValue, expdays) => {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path = /";
+}
+
+getCookie = (cName) => {
+    const name = cName + "=";
+    const cDecoded = decodedURIComponent(document.cookie);
+    const cArr = cDecoded.split("; ");
+    let value;
+
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) value = val.substring(name.length);
+    })
+
+    return value;
+}
+
+document.querySelector("#cookies-btn").addEventListener("click", () => {
+    document.querySelector("#cookies").style.display = "none";
+    setCookie("cookie", true, 30);
+})
+
+cookieMessage = () => {
+    if (!getCookie("cookie"))
+        document.querySelector("#cookies").style.display = "block";
+}
+
+if (bar)
 {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
